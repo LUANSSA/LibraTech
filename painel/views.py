a5c053321bf090a10e97ea386507e8e75bc111d8
Luan Souza <54681373+LUANSSA@users.noreply.github.com>
2025-10-31 23:31:34 +0000

    ♻️  Refatoração: Painel, Templates, URLS e Views

--- painel/views.py
+++ painel/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetConfirmView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView
 from django.contrib import messages
 from django.urls import reverse_lazy
 from .form import PainelUsuariosCadastrarFrom
@@ -12,11 +12,12 @@ from revistas.models import Revistas
 from periodicos.models import Periodicos
 from arquivos.models import Arquivos
 
+# ========== PAINEL ==========
 
 # Painel dashboard
 class PainelDashboardView(LoginRequiredMixin, TemplateView):
+    login_url = "painel_site_entrar"
     template_name = "painel/painel_dashboard.html"
-    login_url = "painel_entrar"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -29,9 +30,45 @@ class PainelDashboardView(LoginRequiredMixin, TemplateView):
         context["total_arquivos"] = Arquivos.objects.count()
 
         return context
+    
+# Painel dashboard cadastrar usuários
+class PainelDashboardUsuariosCadastrarView(LoginRequiredMixin, CreateView):
+    login_url = "painel_site_entrar"
+    model = User
+    form_class = PainelUsuariosCadastrarFrom
+    template_name = "painel/painel_dashboard_usuarios_form.html"
+    success_url = reverse_lazy("painel_site_entrar")
 
-# Painel Listar usuários
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # adiciona a mensagem de sucesso
+        messages.success(self.request, "Cadastro realizado com sucesso! Faça login para continuar.")
+        return response
+    
+    def get_success_url(self):
+        # Define para onde redirecionar após o sucesso
+        return reverse_lazy("painel_dashboard_usuarios_listar")
+    
+# Painel dashboard alterar usuários
+class PainelDashboardUsuariosAlterarView(LoginRequiredMixin, UpdateView):
+    login_url = "painel_site_entrar"
+    model = User
+    form_class = PainelUsuariosCadastrarFrom
+    template_name = "painel/painel_dashboard_usuarios_form.html"
+
+    def form_valid(self, form):
+        # Quando o formulário for válido, realiza a atualização
+        response = super().form_valid(form)
+        messages.success(self.request, "Usuário alterado com sucesso!")
+        return response
+    
+    def get_success_url(self):
+        # Define para onde redirecionar após o sucesso
+        return reverse_lazy("painel_dashboard_usuarios_listar")
+
+# Painel dashboard listar usuários
 class PainlDashboardUsuariosListarView(LoginRequiredMixin, ListView):
+    login_url = "painel_site_entrar"
     model = User
     template_name = "painel/painel_dashboard_usuarios_listar.html"
     context_object_name = "usuarios"
@@ -43,13 +80,20 @@ class PainlDashboardUsuariosListarView(LoginRequiredMixin, ListView):
         context["total_usuarios"] = User.objects.count()
 
         return context
+    
+# Painel sair da conta
+class PainelSairView(LogoutView):
+    next_page = "painel_site_entrar"  # redireciona para login após logout
 
-# Cadastrar usuários
-class PainelUsuariosCadastrarView(CreateView):
+
+# ========== SITE ==========
+
+# Painel site cadastrar usuários
+class PainelSiteUsuariosCadastrarView(CreateView):
     model = User
     form_class = PainelUsuariosCadastrarFrom
-    template_name = "painel/painel_usuarios_form.html"
-    success_url = reverse_lazy("painel_entrar")
+    template_name = "painel/painel_site_usuarios_form.html"
+    success_url = reverse_lazy("painel_site_entrar")
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -57,9 +101,9 @@ class PainelUsuariosCadastrarView(CreateView):
         messages.success(self.request, "Cadastro realizado com sucesso! Faça login para continuar.")
         return response
 
-# Painel login
-class PainelEntrarView(LoginView):
-    template_name = "painel/painel_entrar.html"
+# Painel site login
+class PainelSiteEntrarView(LoginView):
+    template_name = "painel/painel_site_entrar.html"
     redirect_authenticated_user = True  # já redireciona usuários logados automaticamente
 
     # Redireciona para página de dashboard
@@ -71,17 +115,13 @@ class PainelEntrarView(LoginView):
         messages.error(self.request, "Usuário ou senha incorretos.")
         return super().form_invalid(form)
 
-# Painel recuperar senha
-class PainelRecuperarSenhaView(PasswordResetView):
-    template_name = "painel/painel_recuperar_senha.html"
+# Painel site recuperar senha
+class PainelSiteRecuperarSenhaView(PasswordResetView):
+    template_name = "painel/painel_site_recuperar_senha.html"
     email_template_name = "painel/emails/reset_password_email.html"
-    success_url = reverse_lazy("painel_entrar")
+    success_url = reverse_lazy("painel_site_entrar")
 
-# Painel redefinir senha (após o link enviado por e-mail)
-class PainelRedefinirSenhaView(LoginView):
+# Painel site redefinir senha (após o link enviado por e-mail)
+class PainelSiteRedefinirSenhaView(LoginView):
     template_name = "painel/painel_redefinir_senha.html"
-    success_url = reverse_lazy("painel_entrar")
-
-# Painel sair da conta
-class PainelSairView(LogoutView):
-    next_page = "painel_entrar"  # redireciona para login após logout
+    success_url = reverse_lazy("painel_site_entrar")
